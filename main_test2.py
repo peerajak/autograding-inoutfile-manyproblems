edb5e5bd31884817794485d5050c16209994f3a5
peerajak <peerajak@gmail.com>
2022-07-28 16:23:20 +0700

    Update main_test2.py

--- main_test2.py
+++ main_test2.py
@@ -3,10 +3,10 @@ import io
 
 
 def test_problem1(monkeypatch,capsys):  # or use "capfd" for fd-level
-    with open('infile1.txt') as fi:
+    with open('infile2.txt') as fi:
         lines_i = [line for line in fi]
     str_in = "".join(lines_i)
-    with open('outfile1.txt') as fo:
+    with open('outfile2.txt') as fo:
         lines_o = [line for line in fo]
     str_out = "".join(lines_o)
     monkeypatch.setattr('sys.stdin', io.StringIO(str_in))
