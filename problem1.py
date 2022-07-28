88ce1d5d5a438418be272e10a5c240d4a0efc3bf
peerajak <peerajak@gmail.com>
2022-07-28 16:00:21 +0700

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,11 +1,8 @@
-def main():
+def p1():
     inlist = input().split()
     inlist2 = input().split()
     inlist_all = inlist + inlist2
     inlist_all.sort()
     print(" ".join(inlist_all))
 
-
-if __name__ == "__main__":
-    main()
 
