33bd72070efd70ff24209f809a313d36859b8bc4
peerajak <peerajak@gmail.com>
2022-07-28 16:12:34 +0700

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -1,5 +1,4 @@
 from problem1 import p1
-from problem2 import p2
 import io
 
 
@@ -16,14 +15,3 @@ def test_problem1(monkeypatch,capsys):  # or use "capfd" for fd-level
     assert captured.out == str_out
 
     
-def test_problem2(monkeypatch,capsys):  # or use "capfd" for fd-level
-    with open('infile2.txt') as fi:
-        lines_i = [line for line in fi]
-    str_in = "".join(lines_i)
-    with open('outfile2.txt') as fo:
-        lines_o = [line for line in fo]
-    str_out = "".join(lines_o)
-    monkeypatch.setattr('sys.stdin', io.StringIO(str_in))
-    p2() 
-    captured = capsys.readouterr()
-    assert captured.out == str_out
