ed8a1acc29328ebf1f1a70f1ca54a72344a93165
peerajak <peerajak@gmail.com>
2022-07-28 16:14:10 +0700

    Create main_test2.py

---
+++ main_test2.py
@@ -0,0 +1,15 @@
+from problem2 import p2
+import io
+
+
+def test_problem1(monkeypatch,capsys):  # or use "capfd" for fd-level
+    with open('infile1.txt') as fi:
+        lines_i = [line for line in fi]
+    str_in = "".join(lines_i)
+    with open('outfile1.txt') as fo:
+        lines_o = [line for line in fo]
+    str_out = "".join(lines_o)
+    monkeypatch.setattr('sys.stdin', io.StringIO(str_in))
+    p2() 
+    captured = capsys.readouterr()
+    assert captured.out == str_out
